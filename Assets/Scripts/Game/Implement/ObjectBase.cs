ebb7afc98e571e9fde95a1b7d4d6926603e05554
shugen <3093083636@qq.com>
2019-07-03 13:04:53 +0800

    update script file.

---
+++ Assets/Scripts/Game/Implement/ObjectBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBase : MonoBehaviour
+{
+    public int Id { get { return id; } }
+    protected int id;
+
+    private static int dbId;
+
+    private void Awake()
+    {
+        id = dbId++;
+    }
+
+    public static T FactoryCreate<T>() where T : ObjectBase
+    {
+        T t = new GameObject().AddComponent<T>();
+        var name = string.Format("{0}_{1}", typeof(T).Name, t.id);
+        t.name = name;
+        return t;
+    }
+}
---
+++ Assets/Scripts/Game/Manager/EntityManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam2019
+{
+    public class EntityManager
+    {
+        public static EntityManager Inst { get { return inst; } }
+        private static EntityManager inst = new EntityManager();
+
+        private Dictionary<int, EntityBase> entityDict = new Dictionary<int, EntityBase>();
+    }
+}
